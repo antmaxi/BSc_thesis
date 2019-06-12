9237b3eaaa0d158289317b01341248aaa803ccd3
Anton Maksimov <antmaxi@ya.ru>
2019-06-12 20:02:41 +0300

    Added extracting of SMILES of ligands from DB and PubChem, exxtracting of targets' names

--- DATABASES_SMILES.py
+++ DATABASES_SMILES.py
@@ -56,38 +56,7 @@ def get_pdbs_from_uniprot(uniprot, path_to_save=None):
             r = download_url(url, path_list_of_pdbs, name)
             open(full_name, 'wb').write(r.encode('utf-8'))
     return ''.join(r).split()
-
-
-def get_all_smiles_from_pubchem_ids(ligands_ids, ligands_resources):
-    """ Get all SMILEs of compounds in the ligands_ids list
-    https://pubchempy.readthedocs.io/en/latest/guide/install.html
-    """
-    pubchem_ids = []
-    pubchem_smiles = []
-    pubchem_numbers = []
-    for ind1, lig_res in enumerate(ligands_resources):
-        # !! Works too long if many requests
-        if ind1 == 5: #-1 for normal work
-            pass
-            #break
-        else:
-            for ind2, resource in enumerate(lig_res):
-                if resource == 'PubChem Substance':
-                    try:
-                        # Get correspondent PubCHEM id 
-                        pubchem = ligands_ids[ind1][ind2]
-                        # Get smiles from this id
-                        c = pubchempy.Compound.from_cid(pubchem)
-                        pubchem_smiles.append(c.isomeric_smiles)
-                        # Add to list places of successfull ligands in whole list
-                        pubchem_numbers.append(ind1)  
-                        # Add to list of correspondent pubchem ids 
-                        pubchem_ids.append(pubchem) 
-                    except:
-                        # !! What to do if no SMILES?
-                        pass            
-                    break
-    return pubchem_smiles, pubchem_numbers, pubchem_ids              
+             
                     
 def get_pdbs_from_smiles(smiles, step_or_exact=-0.05, name='list_of_pdbs', root_path=ROOT_PATH):
     """Get list of pdbs containing similiar SMILES.
@@ -155,7 +124,7 @@ def get_targets_uniprots_from_ligand_name(name_lig, ligands_names_and_their_targ
 
 
 
-def get_smiles_from_name(name, ligands_ids_by_names, ligands_resources_by_names):
+def get_smiles_from_name_from_pubchem(name, ligands_ids_by_names, ligands_resources_by_names):
     """Return SMILES using usual name of drug in Drugbank
     ligands_ids_by_names -- Dictionary name:list of ids, made by Drugbank processing and dump
     ligands_resources_by_names -- Dictionary name:list of resources, made by Drugbank processing and dump 
@@ -171,15 +140,14 @@ def get_smiles_from_name(name, ligands_ids_by_names, ligands_resources_by_names)
         return smiles
     except ValueError:
         print(f'{name} doesn\'t have pubchem id')
-        
-        
-    
+        return None
+
 
 def get_common_pdbs_from_ligand_name_and_target_uniprot(name_lig, uniprot, sim,
                                                        ligands_ids_by_names, ligands_resources_by_names,
                             ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources):
     """Get list of pdbs which are in both lists of pdbs 
-    INPUT -- ligand's name name_lig (SMILES searched by sim, see function get_smiles_from_name)
+    INPUT -- ligand's name name_lig (SMILES searched by sim, see function get_smiles_from_name_from_pubchem)
     uniprot of target
     OUTPUT -- [(name_lig, uniprot), [list of common pdbs]]
     """
@@ -188,7 +156,7 @@ def get_common_pdbs_from_ligand_name_and_target_uniprot(name_lig, uniprot, sim,
     #global ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources
     
     # Get pdbs lists for ligand and target
-    smiles_of_ligand = get_smiles_from_name(name_lig, ligands_ids_by_names, ligands_resources_by_names)
+    smiles_of_ligand = get_smiles_from_name_from_pubchem(name_lig, ligands_ids_by_names, ligands_resources_by_names)
     pdbs_of_ligand = get_pdbs_from_smiles(smiles_of_ligand, sim)
     pdbs_of_target = get_pdbs_from_uniprot(uniprot)
     
@@ -210,7 +178,7 @@ def get_common_pdbs_with_all_targets_of_ligand(name_lig, sim,
                                                        ligands_ids_by_names, ligands_resources_by_names,
                             ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources):
     """Get list of all pdbs connecting ligand and its target
-    INPUT -- ligand's name is name_lig (SMILES searched by sim, see function get_smiles_from_name)
+    INPUT -- ligand's name is name_lig (SMILES searched by sim, see function get_smiles_from_name_from_pubchem)
     OUTPUT -- dictionary {[lig_name, uniprot]:[list of common pdbs]}
     """
     # Initializing needed global variables
@@ -218,7 +186,7 @@ def get_common_pdbs_with_all_targets_of_ligand(name_lig, sim,
     #global ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources
     
     # Get pdbs lists for ligand and target
-    smiles_of_ligand = get_smiles_from_name(name_lig, ligands_ids_by_names, ligands_resources_by_names)
+    smiles_of_ligand = get_smiles_from_name_from_pubchem(name_lig, ligands_ids_by_names, ligands_resources_by_names)
     pdbs_of_ligand = get_pdbs_from_smiles(smiles_of_ligand, sim)
     uniprots_of_targets = get_targets_uniprots_from_ligand_name(name_lig, ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources)
     
--- Drugbank.py
+++ Drugbank.py
@@ -1,5 +1,4 @@
 """Get needed info from Drugbank, save it. Possible to load info and update the database"""
-
 import os
 import json
 import xml.etree.ElementTree as etree
@@ -7,11 +6,12 @@ import xml.etree.ElementTree as etree
 from pathlib import Path
 import subprocess
 import zipfile
+import pubchempy
 
 import DATABASES_SMILES as db
 
 # Carefully use it: the whole database is 130 MB packed, 1.3 GB unpacked
-def update_drugbank(DRUGBANK_PATH, release='5-1-3'):
+def download_drugbank(DRUGBANK_PATH, release='5-1-3'):
     """Download the whole drugbank database, it usually updates from every 2 weeks to 4 months"""
     # Drugbank authentication data
     NAME = 'your name' #'your name'
@@ -43,39 +43,30 @@ def make_dir(dirList):
 
 def dump_info_db(root):
     """Save all collected from Drugbank data to root/Drugbank_exracted"""
+    # All names of files to be dumped to root/Drugbank_extracted with name.txt, where name is from names
     names = ['ligands_unii', 'ligands_drugbank_ids', 'ligands_names', 'ligands_ids', 'ligands_resources',
              'ligands_ids_by_names', 'ligands_resources_by_names',
              'ligands_names_and_their_targets_ids', 'ligands_names_and_their_targets_resources',
-             'targets_ids', 'targets_resources'
+             'targets_ids', 'targets_resources', 'targets_names',
+             'ligands_smiles', 'ligands_names_and_smiles',
             ]
     name_full = str(Path(root) / 'Drugbank_extracted')
-    make_dir([name_full])
-    
+    make_dir([name_full])    
     for name in names:
         exec('global ' + name)
-    ligands_ids_by_names = dict(zip(ligands_names, ligands_ids))
-    ligands_resources_by_names = dict(zip(ligands_names, ligands_resources))
-    ligands_names_and_their_targets_ids = dict(zip(ligands_names, targets_ids))
-    ligands_names_and_their_targets_resources = dict(zip(ligands_names, targets_resources))
     for name in names:
         with open(str(Path(name_full) / (name + ".txt")), 'w') as f:
             exec('json.dump(' + name + ', f, ensure_ascii=False)')
 
-
-def init_vars_of_drugbank():
-    """Initialise global variables used when processing drugbank data"""
-    global ligands_unii, ligands_drugbank_ids, ligands_names, ligands_ids, ligands_resources
-    global ligands_ids_by_names, ligands_resources_by_names
-    global ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources
-    global targets_ids, targets_resources
-    
     
 def load_info_db(root):
     """Load all collected from Drugbank data from root/Drugbank_exracted"""
+    # All names of files to be loaded from root/Drugbank_extracted with name.txt, where name is from names
     names = ['ligands_unii', 'ligands_drugbank_ids', 'ligands_names', 'ligands_ids', 'ligands_resources',
              'ligands_ids_by_names', 'ligands_resources_by_names',
              'ligands_names_and_their_targets_ids', 'ligands_names_and_their_targets_resources',
-             'targets_ids', 'targets_resources'
+             'targets_ids', 'targets_resources', 'targets_names',
+             'ligands_smiles', 'ligands_names_and_smiles',
             ]
     name_full = str(Path(root) / 'Drugbank_extracted')
 
@@ -90,8 +81,10 @@ def db_tag(element, string):
 
 
 def process_drugbank(root, name='full database.xml'):
-    """Get needed info from the full Drugbank database, placed in root with name"""
-    # !!! Check the first doubled entry
+    """Get needed info from the full Drugbank database, placed in root with name
+    https://www.drugbank.ca/docs/drugbank.xsd -- scheme of the base
+    """
+    # Location of the database
     source = str(Path(root) / name)
     # Get an iterable
     context = etree.iterparse(source, events=("start", "end"))
@@ -104,23 +97,38 @@ def process_drugbank(root, name='full database.xml'):
 
     # Here go lists with collected information about approved by FDA ligands in Drugbank
     # !!! Maybe use some OOP instead of list of lists
-    init_vars_of_drugbank()
+    
+    # Initialize variables for ligands
+    global ligands_unii, ligands_drugbank_ids, ligands_names, ligands_ids, ligands_resources
+    global ligands_ids_by_names, ligands_resources_by_names
+    # Forligands and their targets
+    global ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources
+    global ligands_smiles, ligands_names_and_smiles
+    # For targets
+    global targets_ids, targets_resources, targets_names
+    
     
     ligands_unii = []  # List of ligands' UNII ids
     ligands_drugbank_ids = []  # List of lists of Drugbank ids (one ligand could have several)
     ligands_names = []  # List of usual names
     ligands_ids = []  # List of lists of ids in different DBs
     ligands_resources = []  # List of lists of resources in different DBs
-    ligands_ids_by_names = []  # Dictionary name:list of ids
-    ligands_resources_by_names = []  # Dictionary name:list of resources
+    #ligands_ids_by_names = []  # Dictionary name:list of ids
+    #ligands_resources_by_names = []  # Dictionary name:list of resources
+    ligands_smiles = []  #list of all SMILES of ligands
+    smiles = None
     
     targets_ids = []  # List of lists of lists of ids in different DBs
     targets_resources = []  # List of lists of lists of resources in different DBs
+    targets_names = []  # List lists of names of all targets
     unii = ""
     # Flag of FDA approval
     fda_approved = False
+    # Flag of having SMILES
+    f_smiles = False
+    smiles = None
     # Database iteration (DB is too big to parse it directly)
-    # https://www.drugbank.ca/docs/drugbank.xsd -- scheme of the base
+   
     for event, elem in context:
         # Find drugs storage
         if event == "end" and db_tag(elem,"drug"):        
@@ -133,7 +141,9 @@ def process_drugbank(root, name='full database.xml'):
             t_id = []  # Id of one target in one database
             t_ids = []  # List of ids of one target in all databases
             t_resource = []  # Resource of one target (name of one database) 
-            t_resources = []  # List of resources of one target (names of all databases)        
+            t_resources = []  # List of resources of one target (names of all databases)
+            t_name = []  # Name of one target
+            t_names = []  # List of names of all targets
 
             # Flag of FDA approval
             fda_approved = True
@@ -151,7 +161,19 @@ def process_drugbank(root, name='full database.xml'):
                     l_db_ids.append(item.text)
                 if db_tag(item, "name"):
                     name = item.text
-
+                    
+                # Get SMILES
+                if db_tag(item, 'calculated-properties'):
+                    for item1 in list(item):
+                        for item2 in list(item1):
+                            #print(item2.tag.split('}')[1])
+                            if db_tag(item2, 'kind'):
+                                if item2.text == 'SMILES':
+                                    f_smiles = True
+                            if db_tag(item2, 'value') and f_smiles:
+                                f_smiles = False
+                                smiles = item2.text
+                            
                 # Checking if drug is approved by FDA
                 if db_tag(item, "products"):
                     for it1 in list(item):
@@ -177,6 +199,8 @@ def process_drugbank(root, name='full database.xml'):
                 if db_tag(item, "targets"):
                     for it1 in list(item):
                         for it2 in list(it1):
+                            if db_tag(it2, 'name'):
+                                t_name = it2.text
                             if db_tag(it2, "polypeptide"):
                                 for it3 in list(it2):
                                     if db_tag(it3, "external-identifiers"):
@@ -191,6 +215,8 @@ def process_drugbank(root, name='full database.xml'):
                                         t_id = []
                                         t_resources.append(t_resource)
                                         t_resource = []
+                                        t_names.append(t_name)
+                                        t_name = []
 
             # In order not to store the whole database in memory    
             root_tree.clear()
@@ -203,27 +229,87 @@ def process_drugbank(root, name='full database.xml'):
             ligands_ids.append(l_ids)
             ligands_drugbank_ids.append(l_db_ids)
             ligands_resources.append(l_resources)
+            ligands_smiles.append(smiles)
+            smiles = None
 
             targets_ids.append(t_ids)
             targets_resources.append(t_resources)
-            
+            targets_names.append(t_names)
+    
+    # Create some useful dictionaries
+    ligands_ids_by_names = dict(zip(ligands_names, ligands_ids))
+    ligands_resources_by_names = dict(zip(ligands_names, ligands_resources))
+    ligands_names_and_their_targets_ids = dict(zip(ligands_names, targets_ids))
+    ligands_names_and_their_targets_resources = dict(zip(ligands_names, targets_resources))
+    ligands_names_and_smiles = dict(zip(ligands_names, ligands_smiles))
+    
     # Save obtained data        
     dump_info_db(root)
+    
+
+def add_smiles_from_pubchem(root, ligands_names_and_smiles):
+    """Add SMILES of ligands which don't have one in Drugbank but have it in PubChem (~10 ligands)"""
+    # Needed lists with data
+    # Iterating over names, finding ones without SMILES and trying to get SMILES from PubChem    
+    for name in ligands_names:
+        if not ligands_names_and_smiles[name]:
+            try:
+                ind_compound = ligands_resources_by_names[name].index('PubChem Compound')
+                # Find SMILES
+                pubchem = ligands_ids_by_names[name][ind_compound]
+                # Get smiles from PubCHEM
+                c = pubchempy.Compound.from_cid(pubchem)
+                smiles = c.isomeric_smiles
+                ligands_smiles[ligands_names.index(name)] = smiles
+            except ValueError:
+                pass
+    # Save corrected data
+    #global ligands_names_and_smiles
+    ligands_names_and_smiles = dict(zip(ligands_names, ligands_smiles))
+    name_full = str(Path(root) / 'Drugbank_extracted')
+    with open(str(Path(name_full) / ('ligands_smiles' + '.txt')), 'w') as f:
+            json.dump(ligands_smiles, f, ensure_ascii=False)
+    with open(str(Path(name_full) / ('ligands_names_and_smiles' + ".txt")), 'w') as f:
+            json.dump(ligands_names_and_smiles, f, ensure_ascii=False)
 
 
-# Directory where all data placed
-root = '/media/anton/b8150e49-6ff0-467b-ad66-40347e8bb188/anton/BACHELOR'
-ROOT_PATH = '/media/anton/b8150e49-6ff0-467b-ad66-40347e8bb188/anton/BACHELOR'
-DRUGBANK_PATH = Path(root) / 'Drugbank'
+if __name__ == "__main__":
+    
+    # Initialize variables for ligands
+    global ligands_unii, ligands_drugbank_ids, ligands_names, ligands_ids, ligands_resources
+    global ligands_ids_by_names, ligands_resources_by_names
+    # Forligands and their targets
+    global ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources
+    global ligands_smiles, ligands_names_and_smiles
+    # For targets
+    global targets_ids, targets_resources, targets_names
+    
+    # Directory where all data placed
+    root = '/media/anton/b8150e49-6ff0-467b-ad66-40347e8bb188/anton/BACHELOR'
+    ROOT_PATH = '/media/anton/b8150e49-6ff0-467b-ad66-40347e8bb188/anton/BACHELOR'
+    DRUGBANK_PATH = Path(root) / 'Drugbank'
 
-# If needed to update:
-#process_drugbank(ROOT_PATH)
-#update_drugbank(DRUGBANK_PATH)
-load_info_db(root)
-
-
-
-name_lig = 'Methazolamide'
-uniprot = 'P00918'
-#print(get_common_pdbs_from_ligand_name_and_target_uniprot(name_lig, uniprot, 0.4))
-print(get_common_pdbs_with_all_targets_of_ligand(name_lig, 0.4))
+    # If needed to update:
+    #update_drugbank(DRUGBANK_PATH)
+    
+    # Processing if new information needed or wasn't dumped before
+    process_drugbank(root)
+    # Add SMILES of ligands which don't have one in Drugbank but have it in PubChem
+    add_smiles_from_pubchem(root, ligands_names_and_smiles)
+    
+    # Start of user actions
+    # Load data from .txts from root/Drugbank_extracted to program
+    load_info_db(root)
+    
+    # Some checks of work
+    #print(ligands_ids_by_names)
+    name_lig = 'Acetazolamide'#'Methazolamide'  #'Acetaminophen' 'Acetazolamide' 
+    uniprot = 'P00918'
+    #a = get_all_smiles_of_ligands(ligands_ids_by_names, ligands_resources_by_names)
+    #print(db.get_pdbs_from_smiles(sm, 0.5))
+    #print(db.get_common_pdbs_from_ligand_name_and_target_uniprot(name_lig, uniprot, -0.1,
+    #                                                  ligands_ids_by_names, ligands_resources_by_names,
+    #                       ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources))
+    #print(db.get_common_pdbs_with_all_targets_of_ligand(name_lig, -0.1,
+    #                                                  ligands_ids_by_names, ligands_resources_by_names,
+    #                       ligands_names_and_their_targets_ids, ligands_names_and_their_targets_resources))
