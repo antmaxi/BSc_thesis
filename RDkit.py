b5623ee37360356f173cc768632df54eb925772e
Anton Maksimov <antmaxi@ya.ru>
2019-06-14 19:59:34 +0300

    Fixed sequence sim calc, added SMILES sim calc and search

--- RDkit.py
+++ RDkit.py
@@ -9,37 +9,67 @@ import subprocess
 from rdkit import Chem
 from rdkit import DataStructs
 from rdkit.Chem.Fingerprints import FingerprintMols
-from Bio import pairwise2
-from Bio import SeqIO
-#from Bio.SubsMat.MatrixInfo import *
-import Bio.SubsMat.MatrixInfo
 
-def get_fasta_similiarity(name1, name2):
-    """Calculates similiarity of two sequences in fasta using blosum62 matrix and paths to fastas
-    Input - paths to fasta sequences
-    Output - similiarity and identity
+from Bio import pairwise2
+import Bio.SubsMat.MatrixInfo
+from Bio.SubsMat.MatrixInfo import *
+# Doesn't work
+#from Bio import SeqIO
+
+
+class get_seq_from_fasta:
+    """Get name and string from .fasta (biopython's SeqIO sometimes doesn't work:( ))"""
+    def __init__(self, path):
+        with open(path, 'r') as f:
+            seq = ''
+            for line in f.readlines():
+                if line[0] == '>':
+                    name = line.rstrip()
+                else:
+                    seq += line.rstrip()
+            self.name = name
+            self.seq = seq
+
+
+def get_sequences_similiarity(input1, input2):
+    """Calculates similiarity of two sequences using blosum62 matrix and 
+    paths to fastas or sequences
+    Input - paths to fasta sequences or these sequences
+    Output - integers similiarity and identity
     """
+    # Draft for using different sucstitution matrices
     #print('Available matrices:', Bio.SubsMat.MatrixInfo.available_matrices)
     #print('Which one would you like to use? Type [Enter] to use blosum62')
     #align_matr = input()
     #if align_matr == "":
     align_matr = "blosum62"
-    print(align_matr)
-    seq1 = SeqIO.read(name1, "fasta")
-    seq2 = SeqIO.read(name2, "fasta")
-    alignments = pairwise2.align.globalds(seq1.seq, seq2.seq, blosum62, -10, -0.5)
-    alignments_id = pairwise2.align.globalms(seq1.seq, seq2.seq, 1, 0, 0, 0)
+    print(f'Using {align_matr}')
+    # If input is paths to fasta
+    if input1.split('.')[-1] == 'fasta':
+        # If import from Bio import SeqIO works
+        #seq1 = Bio.SeqIO.read(name1, "fasta")
+        #seq2 = Bio.SeqIO.read(name2, "fasta")
+        seq1 = get_seq_from_fasta(input1)
+        str1 = seq1.seq
+        seq2 = get_seq_from_fasta(input2)
+        str2 = seq2.seq
+    # If input is strings
+    else:
+        str1 = input1
+        str2 = input2
+    alignments = pairwise2.align.globalds(str1, str2, blosum62, -10, -0.5)
+    alignments_id = pairwise2.align.globalms(str1, str2, 1, 0, 0, 0)
     print("Number of alignments = " + str(len(alignments)))
     print(pairwise2.format_alignment(*alignments[0]))
-    sim = alignments[0][2]
-    ident = str(alignments_id[0][2]).split('.')[0]
+    sim = int(alignments[0][2])
+    ident = int(str(alignments_id[0][2]).split('.')[0])
     return sim, ident
 
 
 def get_TMscore_and_RMSD(pdb1_path, pdb2_path):
     """Returns (TM-score, RMSD) of two pdbs, whose paths are in INPUT
-    0.0 < TM-score < 0.17, random structural similarity                 *
-    0.5 < TM-score < 1.00, in about the same fold 
+    if 0.0 < TM-score < 0.17, then random structural similarity 
+    if 0.5 < TM-score < 1.00, then in about the same fold 
     Returns 0.0 if no common residues were found
     """
     res = subprocess.check_output(['TMscore', pdb1_path, pdb2_path])
@@ -54,49 +84,58 @@ def get_TMscore_and_RMSD(pdb1_path, pdb2_path):
     #print(text[ind_tm:ind_tm+40].split()[2])
     
 
-def get_SMILES_sim():
-    """!!! fix the function"""
-    # Read and Concaonate the csv's
-    df = pd.read_csv('first.csv')
-
+def get_smiles_similiarity(smiles, list_smiles):
+    """Get dictionary sims:smiles_from_list for smiles comparing list_smiles"""
     # Proof and make a list of SMILES
-    df_smiles = df['smiles']
     c_smiles = []
-    for ds in df_smiles:
+    for ds in list_smiles:
+        print(ds)
         try:
             cs = Chem.CanonSmiles(ds)
             c_smiles.append(cs)
         except:
             print('Invalid SMILES:', ds)
-    print()
+    try:
+        smiles = Chem.CanonSmiles(smiles)
+    except:
+        print('Invalid SMILES:', ds)    
 
     # Make a list of mols
     ms = [Chem.MolFromSmiles(x) for x in c_smiles]
 
     # Make a list of fingerprints (fp)
     fps = [FingerprintMols.FingerprintMol(x) for x in ms]
+    # Input fingerprint
+    fp_in = FingerprintMols.FingerprintMol(Chem.MolFromSmiles(smiles))
 
-    # The list for the dataframe
-    qu, ta, sim = [], [], []
-
-    # Compare all fp pairwise without duplicates
-    for n in range(len(fps)-1): # -1 so the last fp will not be used
-        s = DataStructs.BulkTanimotoSimilarity(fps[n], fps[n+1:]) # +1 compare with the next to the last fp
-        print(c_smiles[n], c_smiles[n+1:]) # witch mol is compared with what group
-        # Collect the SMILES and values
-        for m in range(len(s)):
-            qu.append(c_smiles[n])
-            ta.append(c_smiles[n+1:][m])
-            sim.append(s[m])
-    print()
+    # Compare all fps with fp_in
+    sim = (DataStructs.BulkTanimotoSimilarity(fp_in, fps[:])) # +1 compare with the next to the last fp
+    #print()
 
     # Build the dataframe and sort it
-    d = {'query':qu, 'target':ta, 'Similarity':sim}
+    d = {'query':smiles, 'target_smiles':list_smiles, 'similarity':sim}
     df_final = pd.DataFrame(data=d)
-    df_final = df_final.sort_values('Similarity', ascending=False)
-    print(df_final)
-
-    # Save as csv
-    df_final.to_csv('result.csv', index=False, sep=',')
+    df_final = df_final.sort_values('similarity', ascending=False)
+    return df_final#dict(zip(df_final['Similarity'], df_final['target']))
     
+
+def get_closest_smiles_name(smiles, ligands_names_and_smiles, k=1):
+    """Get k names and smiles of the closest to input smiles, k=1 by default"""
+    res = get_SMILES_similiarity(smiles, list(ligands_names_and_smiles.values()))
+    result = res[0:k]
+    names = []
+    for sm in result['target_smiles']:
+        for name in ligands_names_and_smiles.keys():
+            if ligands_names_and_smiles[name] == sm:
+                names.append(name)
+    result['Name'] = names
+    return result
+
+# Tests
 #print(get_TMscore_and_RMSD('hive/pdb/1A3B.pdb', 'hive/pdb/1RDQ.pdb')) #1MD7 1RDQ
+#get_SMILES_sim()
+#print(get_sequences_similiarity('RDkit/O14733.fasta', 'RDkit/P00533.fasta'))
+#print(get_sequences_similiarity('ABB', 'ABC'))
+#a = get_SMILES_similiarity('ClCCNC(=O)N(CCCl)N=O', 
+#                       ('CN1CCC[C@@H]1CCO[C@](C)(C1=CC=CC=C1)C1=CC=C(Cl)C=C1', 'ClCCNC(=O)N(CCCl)N=O'))
+#print(a)
